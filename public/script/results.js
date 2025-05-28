5b3ed3c1b359b51b5d6a0e3db514a47ca449ffe3
Dziad Chlebowy <baraniaklukasz3@gmail.com>
2025-05-28 14:59:09 +0200

    liczba lajkow w szukajce

--- public/script/results.js
+++ public/script/results.js
@@ -14,8 +14,9 @@ const postHTML = `<div class="post">
       <p class="post-text">{KONTENT}</p>
   </div>
   <div class="post-actions">
-      <button class="btn-like"><i class="fas fa-thumbs-up" aria-label="Like"></i></button>
-      <button class="btn-save"><i class="fas fa-bookmark" aria-label="Save"></i></button>
+      <button class="btn-like{B}" type="button" id="{ID}" aria-label="Like"><i class="fas fa-thumbs-up"> {LAJKI}</i>
+</button>
+      <button class="btn-save"><i class="fas fa-bookmark"aria-label="Save"></i></button>
   </div>
 </div>`
 
@@ -25,6 +26,15 @@ const postHTML = `<div class="post">
 async function zaladujPosty() {
   const posts = await fetch('http://localhost:3000/posts')
     .then(resp => resp.json())
+
+  const lajki = document.querySelectorAll('.btn-like, .btn-like-blue')
+  if (!lajki) return
+  Array.from(lajki).forEach(lajk => {
+    lajk.onclick = () => {
+      polajkuj(lajk)
+    }
+  })
+
   const query = location.search.split('?search=')[1]
   let index = 0
   posts.forEach(post => {
@@ -37,6 +47,28 @@ async function zaladujPosty() {
     document.getElementById('resultsHeader').textContent = `Ups! Nie znaleźliśmy żadnych wyników.`
   } else {
     document.getElementById('resultsHeader').textContent = `Znalezione wyniki (${index}):`
+  }
+}
+
+async function polajkuj(button) {
+
+  const userUID = sessionStorage.getItem('userUID')
+  const postID = button.id
+
+  const posts = await fetch('http://localhost:3000/posts')
+    .then(resp => resp.json())
+  const post = posts.filter(post => post.id == postID)[0]
+  if (!post) return alert('Nie udało się zalajkować posta :(')
+  let liked = false
+
+  if (!post.lajkujacy.includes(userUID)){
+    post.lajkujacy.push(userUID);
+    liked = true
+  }
+  else
+  {
+    post.lajkujacy.splice(post.lajkujacy.indexOf(userUID));
+    liked = false
   }
 }
 
@@ -56,11 +88,25 @@ async function dodajPost(post) {
   let autor = authorObject.username
   let data = new Date(post.date).toLocaleDateString()
   let kontent = post.content
+  let lajkujacy = []
+  const userUID = sessionStorage.getItem('userUID')
+  let blue = ""
+  
+    if (!post.lajkujacy.includes(userUID)){
+      blue=""
+    }
+    else
+    {
+      blue="-blue"
+    }
   const kod = postHTML
     .replace('{AWATAR}', avatar)
     .replace('{AUTOR}', autor)
     .replace('{DATA}', data)
     .replace('{KONTENT}', kontent)
+    .replace('{ID}', post.id)
+    .replace('{LAJKI}', post.lajkujacy.length)
+    .replace('{B}', blue)
   document.getElementById('searchPostList').insertAdjacentHTML('afterbegin', kod)
 }
 
